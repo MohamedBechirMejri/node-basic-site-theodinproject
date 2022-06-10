f9ddde38f46748793236eafa37e0c5901c5fb365
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-10 01:57:16 +0100

    get port from process

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 const index = fs.readFileSync("./index.html");
 const contact = fs.readFileSync("./contact-me.html");
